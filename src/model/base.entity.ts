c148b84b14bd4f8aa30e14aa5816b6778d6bbdbf
kraenkthetank <codingTwitter@web.de>
2024-07-12 11:18:36 +0200

    changed base entity

--- src/model/base.entity.ts
+++ src/model/base.entity.ts
@@ -6,28 +6,18 @@ export abstract class BaseEntity{
     @PrimaryGeneratedColumn('uuid')
     id: number
 
-    @Column({type: 'boolean', default: true})
-    isActive: boolean;
-
-    @Column({type: 'boolean', default: false})
-    isArchived: boolean;
-
-    @CreateDateColumn({type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP'})
+    @CreateDateColumn({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
     createDateTime: Date;
 
     @Column({type: 'varchar', length: 300})
     createdBy: string;
 
-    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP'})
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
     lastChangedDateTime: Date;
 
     @Column({type: 'varchar', length: 300})
     lastChangedBy: string;
 
-    @Column({type: 'varchar', length: 300, nullable: true})
-    internalComment: string | null;
-
-    
 
 
 
