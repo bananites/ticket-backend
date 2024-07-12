b6736d9e65aca7ff2f89a42d094bfd2c3abd9786
kraenkthetank <codingTwitter@web.de>
2024-07-12 11:18:05 +0200

    changed config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig} from 'dotenv'
+import { Ticket } from 'src/ticket/entities/ticket.entity';
 import { User } from 'src/user/entities/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
@@ -8,7 +9,7 @@ dotenvConfig({ path: '.dev.env'});
 
 
 const configProd = {
-    type: 'postgres',
+    type: `${process.env.DATABASE_TYPE}`,
     host: `${process.env.DATABASE_HOST}`,
     port: `${process.env.DATABASE_PORT}`,
     username: `${process.env.DATABASE_USERNAME}`,
@@ -20,14 +21,15 @@ const configProd = {
     synchronize: false,
 }
 const config = {
-    type: 'mysql',
+    type: `${process.env.DATABASE_TYPE}`,
     host: `${process.env.DATABASE_HOST}`,
     port: `${process.env.DATABASE_PORT}`,
     username: `${process.env.DATABASE_USERNAME}`,
     password: `${process.env.DATABASE_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
-    entities: [User],
-    migrations: [User],
+    // TODO do i need this? if yes do index entities?
+    // entities: [User],
+    // migrations: [User, Ticket],
     autoLoadEntities: true,
     synchronize: true
 }
