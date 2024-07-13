3c8ce71520fec37ce0e6b4ecc1599b2eb05cde99
kraenkthetank <codingTwitter@web.de>
2024-07-13 10:50:07 +0200

    init commit

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
+import { TicketController } from './ticket.controller';
+import { TicketService } from './ticket.service';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Ticket])]
+    imports:[TypeOrmModule.forFeature([Ticket])],
+    controllers: [TicketController],
+    providers: [TicketService]
 })
 export class TicketModule {}
