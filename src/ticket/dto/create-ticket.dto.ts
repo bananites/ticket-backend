bf22fe45b2ffe754a6672d35f511b14f1cdaf9e9
kraenkthetank <codingTwitter@web.de>
2024-07-27 21:04:30 +0200

    declared schema for ticket

--- src/ticket/dto/create-ticket.dto.ts
+++ src/ticket/dto/create-ticket.dto.ts
@@ -1,15 +1,49 @@
 import { User } from "src/user/entities/user.entity";
 import { TicketStatus } from "../enums";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateTicketDto {
 
+    @ApiProperty({
+        example: "description",
+        required: true
+    })
     description: string
+
     status: TicketStatus
+
+    @ApiProperty({
+        default:"8fa408ef-0ce1-4f41-9674-47aa3246404a",
+        description: "User ID, default",
+        example: "8fa408ef-0ce1-4f41-9674-47aa3246404a",
+        required: false
+    })
     owner: User['id']
+
+    @ApiProperty({
+        default: "TIMESTAMP",
+        required: true
+    })
     createDateTime: Date
+
+    @ApiProperty({
+        default: "TIMESTAMP",
+        required: false
+    })
     lastChangedDateTime: Date
-    createdBy: string
-    lastChangedBy: string
+    
+    @ApiProperty({
+        description: "User ID",
+        required: true
+    })
+    createdBy: User['id']
+    
+    @ApiProperty({
+        default: "createdBy",
+        description: "User ID",
+        required: true
+    })
+    lastChangedBy: User['id']
 
     
 
