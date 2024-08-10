0b145b4a89ddc0e084b8da8f4cfdae59a4d480e6
kraenkthetank <codingTwitter@web.de>
2024-08-10 21:33:44 +0200

    updated import

--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
-import { DataSource, Repository } from 'typeorm';
+import {  Repository } from 'typeorm';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 
 @Injectable()
