4e27e9bda25d16b52b411fed7ca3474f298569ba
kraenkthetank <codingTwitter@web.de>
2024-08-12 20:06:43 +0200

    created schema for user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,35 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { Status } from "../enums"
+import { APP_FILTER } from "@nestjs/core"
 
 
 export class CreateUserDto {
 
+    @ApiProperty({
+        example:"firstname",
+        required: true
+    })
     firstname: string
+
+    @ApiProperty({
+        example: "lastname",
+        required: true
+    })
     lastname: string
+
+    @ApiProperty({
+        example:"example@example.de",
+        required: true
+    })
     email: string
+
+    @ApiProperty({
+        example:"password123!",
+        required: true
+    })
     password: string
+
+    
     status: Status
     createDateTime: Date
     lastChangedDateTime: Date
