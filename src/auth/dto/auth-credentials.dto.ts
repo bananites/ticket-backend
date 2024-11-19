60f8726f81178b9c9240e5c6cf6aa7c40e9fa0ab
bananites <codingtwitter@web.de>
2024-11-19 20:03:13 +0100

    add authCredentialsDto

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthCredentialsDto {
+  @ApiProperty({
+    required: true,
+    default: 'example@example.de',
+  })
+  email: string;
+
+  @ApiProperty({
+    required: true,
+    default: 'password123!',
+  })
+  password: string;
+}
